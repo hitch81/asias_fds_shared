afa784a6e27b8123e59289233ca6c65e9af1bba8
Keith <keithcc1966@gmail.com>
2013-08-29 07:28:10 -0400

    restore previous signature of get_deps() function, handle get_deps_series() independently.

--- staged_helper.py
+++ staged_helper.py
@@ -641,7 +641,7 @@ def get_deps_series(node_class, params, node_mgr, series):
         return deps
 
 
-def get_deps(node_class, params, node_mgr, series, h5flight):
+def get_deps(node_class, params, node_mgr, h5flight):
         # build ordered dependencies
         # 'params' is a dictionary of previously computed nodes
         deps = []
@@ -1083,8 +1083,8 @@ def run_analyzer(short_profile,    module_names,
         aircraft_info['Tail Number'] = registration
         logger.debug(aircraft_info)
         logger.debug(' *** Processing flight %s', flight_file)
-        #try: 
-        if True:
+        #if True:
+        try: 
             derived_nodes_copy = copy.deepcopy(derived_nodes)
             series_copy = series_keys[:]
             with hdf_file(output_path_and_file) as hdf:
@@ -1098,14 +1098,12 @@ def run_analyzer(short_profile,    module_names,
  
             if short_profile=='base': dump_pickles(output_path_and_file, params, kti, kpv, phases, approach, flight_attrs, logger)
             status='ok'
-        '''
         except:
             ex_type, ex, tracebck = sys.exc_info()
             logger.warning('ANALYZER ERROR '+flight_file)
             traceback.print_tb(tracebck)
             status='failed'
             del tracebck                
-        '''    
         logger.info(' *** Processing flight %s finished ' + flight_file + ' time: ' + str(time.time()-file_start_time) + 'status: '+status)
         # reports
         stage = 'analyze' if short_profile=='base' else 'profile'    
